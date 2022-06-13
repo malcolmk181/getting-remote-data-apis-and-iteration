b3ed9478fb876d296265331688e9f69da0c7ca13
Malcolm Keyes <3.malcolmk@gmail.com>
2022-06-13 15:31:44 -0400

    done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,10 +4,15 @@ require 'pry'
 
 def get_character_movies_from_api(character_name)
   #make the web request
-
-
   response_string = RestClient.get('http://swapi.dev/api/people')
   response_hash = JSON.parse(response_string)
+
+  # response_hash['results'] is the array of character hashes
+  # and we want to check the name key of each hash
+
+  film_urls = response_hash['results'].find { |character| character['name'].downcase == character_name }['films']
+  
+  film_hashes = film_urls.map { |url| JSON.parse(RestClient.get(url)) }
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
@@ -22,6 +27,9 @@ end
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+
+  films.each {|film| puts film['title'] }
+
 end
 
 def show_character_movies(character)
